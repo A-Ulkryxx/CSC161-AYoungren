59a3828ab5abff183d747e0a2cb15eaa5663c031
Ulkryxx <Ayoungren94@gmail.com>
2022-04-25 09:39:20 -0600

    HW10 First completion draft

--- Graph/src/main/java/com/CSC161_AYoungren/Graph/Graph.java
+++ Graph/src/main/java/com/CSC161_AYoungren/Graph/Graph.java
@@ -5,156 +5,179 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Queue;
 import java.util.Vector;
 
 public class Graph<V>
 {
-	protected List<V> vertices = new Vector<>();
-	protected List<List<Edge>> neighbors = new Vector<>();
-	
-	public class Edge implements Comparable<Edge>
-	{
-		public int s;
-		public int d;
-		public boolean visit;
-		public double weight;
-		
-		public Edge(int s, int d) //for non-weighted graphs
-		{
-			this.s = s;
-			this.d = d;
-		}
-		
-		public Edge(int s, int d, double weight) //for weighted graphs
-		{
-			this.s = s;
-			this.d = d;
-			this.weight = weight;
-		}
-		
-		@SuppressWarnings("unchecked")
-		public boolean equals(Object edge)
-		{
-			return (s == ((Edge) edge).s) && (d == ((Edge) edge).d);
-		}
+	    protected List<V> vertices = new ArrayList<>();
+	    protected List<List<Edge>> neighbors = new ArrayList<>();
 
+	    public class Edge implements Comparable<Edge> 
+	    {
+	        public int s; // source vertices
+	        public int d; // destination vertices
+	        public double weight; // miles, time to travel, etc...
 
-		@Override
-		public int compareTo(Graph<V>.Edge other) {
-			if(this.weight < other.weight)
-			{
-				return -1;
-			}
-			else if(this.weight > other.weight)
-			{
-				return 1;
-			}
-			return 0;
-		}
-		
-		
-	}
-	
-	public Graph(List<V> vertices, int[][] edges)
+	        public Edge(int s, int d)
+	        {
+	            this.s = s;
+	            this.d = d;
+	        }
+
+	        public Edge(int s, int d, double weight)
+	        {
+	            this.s = s;
+	            this.d = d;
+	            this.weight = weight;
+	        }
+
+	        public boolean equals(Object edge) 
+	        {
+	            return s == ((Edge) edge).s && d == ((Edge) edge).d;
+	        }
+
+	        @Override
+	        public int compareTo(Edge other) 
+	        {
+	            if (this.weight < other.weight) 
+	            {
+	                return -1;
+	            }
+	            else if (this.weight > other.weight)
+	            {
+	                return 1;
+	            }
+	            return 0;
+	        }
+	    }
+
+	    public void MyGraph(List<V> vertices, int[][] edges) 
+	    {
+	        for (V vertex : vertices) 
+	        {
+	            addVertex(vertex);
+	        }
+	        createdAdjacencyLists(edges, vertices.size());
+	    }
+
+	    public void createdAdjacencyLists(int[][] edges, int numberOfVertices) 
+	    {
+	        for (int i = 0; i < edges.length; i++) 
+	        {
+	            addEdge(new Edge(edges[i][0], edges[i][1]));
+	        }
+	    }
+
+	    public boolean addVertex(V vertex) {
+	        if (!vertices.contains(vertex))
+	        {
+	            vertices.add(vertex);
+	            neighbors.add(new ArrayList<Edge>());
+	            return true;
+	        }
+	        else
+	        {
+	            return false;
+	        }
+	    }
+
+	    public boolean addEdge(Edge edge) 
+	    {
+	        if (!neighbors.get(edge.s).contains(edge)) 
+	        {
+	            neighbors.get(edge.s).add(edge);
+	            return true;
+	        }
+	        else
+	        {
+	            return false;
+	        }
+	    }
+
+	    public void printEdges() {
+	        for (int i = 0; i < neighbors.size(); i++) 
+	        {
+	            System.out.print("Vertex: " + i + ":");
+	            for (Edge edge : neighbors.get(i))
+	            {
+	                System.out.print("(" + edge.s + ", " + edge.d + ")");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+	  		//use a queue
+	public List<V> bfs(Integer vert)
 	{
-		for(V vert: vertices)
-		{
-			addVertex(vert);
-		}
-		createAdjacencyLists(edges, vertices.size());
-	}
-	
-	public boolean addVertex(V vert)
-	{
-		if(!vertices.contains(vert))
-		{
-			vertices.add(vert);
-			neighbors.add(new Vector<Edge>());
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	
-	public boolean addEdge(Edge edge)
-	{
-		if(!neighbors.get(edge.s).contains(edge))
-		{
-			neighbors.get(edge.s).add(edge);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	private void createAdjacencyLists(int[][] edges, int numVertices)
-	{
-		for(int i = 0; i < edges.length;i++)
-		{
-			addEdge(new Edge(edges[i][0], edges[i][1]));
-		}
-	}
-	
-	public void printEdges()
-	{
-		for(int i = 0; i < neighbors.size();i++)
-		{
-			System.out.println("Vertex " + i + ":");
-			for(Edge e: neighbors.get(i))
-			{
-				System.out.print("(" + e.s + ", " + e.d + ")");
-			}
-			System.out.println();
-		}
-	}
-	
-//	public List<V> bfs()
-//	{
-//		//TODO Breadth First
-//		//use a queue
-//	}
-	
-	
-			//include an input as starting point 
-			//use a stack
-			//no recursion
-	public List<V> dfs(int vert)
-	{
-		Deque<V> stack = new ArrayDeque<V>();
-		List<V> print = new ArrayList<V>();
-		Vector<Boolean> visited = new Vector<Boolean>(vertices.size());
+		Queue<Integer> queue = new ArrayDeque<>();
+		List<Integer> print = new ArrayList<>();
+		ArrayList<Boolean> visited = new ArrayList<Boolean>();
 		for(int i = 0; i < vertices.size(); i++)
 		{
 			visited.add(false);
 		}
-		stack.push(vertices.get(vert));
 		
-		while(stack.isEmpty() == false)
+		//visited.set(vert, true);
+		queue.add(vert);
+		visited.set(vert, true);
+		
+		while(!queue.isEmpty())
 		{
-			v = stack.;
-			print.add(stack.pop());
-			
-			if(visited.get(v) == false)
+			vert = queue.peek();
+			print.add(queue.poll());
+			//List<Graph<V>.Edge> neighList = neighbors.get(vert);
+			for(int i = neighbors.size() - 1; i >= 0; i--)
 			{
-				visited.add(v, true);
-			}
-			
-			@SuppressWarnings("unchecked")
-			Iterator<List<Graph<V>.Edge>> iterator = neighbors.iterator();
-			if(iterator.hasNext())
-			{
-				v = (int) iterator.next();
-				if(!visited.get(v))
+				if(visited.get(i) == false)
 				{
-					stack.push(vertices.get(v));
+					visited.set(i, true);
+					queue.add(i);
 				}
 			}
 		}
-		return print;
+		
+		return (List<V>) print;
+	}
+	
+			//include an input as starting point 
+			//use a stack
+			//no recursion
+	public List<V> dfs(Integer vert)
+	{
+		Deque<Integer> stack = new ArrayDeque<>();
+		List<Integer> print = new ArrayList<>();
+		ArrayList<Boolean> visited = new ArrayList<Boolean>();
+		for(int i = 0; i < vertices.size(); i++)
+		{
+			visited.add(false);
+		}
+		stack.push(vert);
+		
+		while(!stack.isEmpty())
+		{
+			vert = stack.peek();
+			
+			if(visited.get(vert) == false)
+			{
+				print.add(stack.pop());
+				visited.set(vert, true);
+			}
+			else
+			{
+				stack.pop();
+			}
+			
+			List<Graph<V>.Edge> neighList = neighbors.get(vert);
+			for(int i = neighList.size() - 1; i >= 0; i--)
+			{
+				int v = neighList.get(i).d;
+				if(!visited.get(v))
+				{
+					stack.push(v);
+				}
+			}
+		}
+		return (List<V>) print;
 	}
 }
--- Graph/src/main/java/com/CSC161_AYoungren/Graph/GraphTester.java
+++ Graph/src/main/java/com/CSC161_AYoungren/Graph/GraphTester.java
@@ -1,24 +1,45 @@
 package com.CSC161_AYoungren.Graph;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class GraphTester {
 
 	public static void main(String[] args) {
-		List<Vertex> vertices = new Vector<>();
+		List<Vertex> vertices = new ArrayList<Vertex>();
 		for(int i = 0; i < 5; i++)
 		{
 			vertices.add(new Vertex(i));
 		}
 
-		int[][]edges = { {0,1}, {1,3}, {1,4}, {0,3} };
+		int[][]edges = { {0,1}, {0,4}, {1,3}, {1,4}, {1,2},{2,1}, {2,3}, {3,2}, {3,1}, {3,4}, {4,3}, {4,1}, {4,0} };
 		
-		Graph<Vertex> graph1 = new Graph<Vertex>(vertices, edges);
+		Graph<Vertex> graph1 = new Graph<Vertex>();
+		
+		graph1.MyGraph(vertices, edges);
 		
 		graph1.printEdges();
-		
-		graph1.dfs(0);
+		System.out.println("DFS - 0:");
+		System.out.println(graph1.dfs(0).toString());
+		System.out.println("BFS - 0:");
+		System.out.println(graph1.bfs(0).toString());
+		System.out.println("DFS - 1:");
+		System.out.println(graph1.dfs(1).toString());
+		System.out.println("BFS - 1:");
+		System.out.println(graph1.bfs(1).toString());
+		System.out.println("DFS - 2:");
+		System.out.println(graph1.dfs(2).toString());
+		System.out.println("BFS - 2:");
+		System.out.println(graph1.bfs(2).toString());
+		System.out.println("DFS - 3:");
+		System.out.println(graph1.dfs(3).toString());
+		System.out.println("BFS - 3:");
+		System.out.println(graph1.bfs(3).toString());
+		System.out.println("DFS - 4:");
+		System.out.println(graph1.dfs(4).toString());
+		System.out.println("BFS - 4:");
+		System.out.println(graph1.bfs(4).toString());
 	}
 
 }
